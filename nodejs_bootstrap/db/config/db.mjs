e1179c373b6cf1f73fa5213575a950278dd650f4
OmarBueno <omarzaldivar07@aragon.unam.mx>
2023-10-14 14:32:40 -0600

    Se cambio la configuracion de db

--- nodejs_bootstrap/db/config/db.mjs
+++ nodejs_bootstrap/db/config/db.mjs
@@ -1,10 +1,9 @@
 import { Sequelize } from "sequelize";
-const {AZURE_MYSQL_HOST, AZURE_MYSQL_PORT , AZURE_MYSQL_USER , AZURE_MYSQL_PASSWORD } = process.env;
-const db = new Sequelize("hoteles", AZURE_MYSQL_USER , AZURE_MYSQL_PASSWORD, {
-  dialect: "mysql",
+const db = new Sequelize("hoteles", "usuario" , "Password", {
+  dialect: "mariadc",
   dialectOptions: {
-    host: AZURE_MYSQL_HOST,
-    port: AZURE_MYSQL_PORT,
+    host: "host",
+    port: "port",
     timestamps: false,
     underscore: false,
     pool: {
